01cebcf933ff9854f17ade0d1fadd564fc2ad512
RodolfoFadino <rodolfo.fadino@gmail.com>
2018-10-22 18:41:00 -0300

    sync project

--- fp_stack.core/Models/Noticia.cs
+++ fp_stack.core/Models/Noticia.cs
@@ -8,8 +8,9 @@ namespace fp_stack.core.Models
     {
         public int Id { get; set; }
         public string Titulo { get; set; }
-        public string Link { get; set; }
-        public Autor Autor { get; set; }
+        public string Imagem { get; set; }
+        public string DescricaoImagem { get; set; }
+        public string Url { get; set; }
 
     }
 }
